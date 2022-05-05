8161113edf0431c85719db93a260a4b2c58de763
Florian Mabillon <mabillon.flo@gmail.com>
2022-05-05 17:08:51 +0200

    v0.3

---
+++ app/accueil.js
@@ -0,0 +1,6 @@
+let accueil = document.getElementById('accueil')
+// Quand on clique sur la page d'accueil 
+accueil.addEventListener('click', redirectionPage)
+function redirectionPage (){
+    document.location.href="a_propos.html";
+}
--- app/app.js
+++ app/app.js
@@ -1,6 +1,14 @@
-let accueil = document.getElementById('accueil')
 
-accueil.addEventListener('click', redirectionPage)
-function redirectionPage (){
-    document.location.href="a_propos.html";
-}
+let toggleMode = document.querySelector('#btn_mode')
+
+
+toggleMode.addEventListener('click', () => {
+    document.body.classList.toggle('light')
+})
+
+// for(i=0; i<scrollDown.length; i++){
+//     toggleMode[i].addEventListener("click", function(){
+//         this.classList.toggle("light");
+//     })
+// }
+console.log(toggleMode);
